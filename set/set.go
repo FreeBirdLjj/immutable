b9f34f2e352274cf3a5f7b3f03237498da5ad552
FreeBirdLjj <ljj11011@mail.ustc.edu.cn>
2023-04-29 15:43:03 +0800

    Rename `immutable_set.NewSetFromValues()` to `immutable_set.FromValues()`.

--- set/set.go
+++ set/set.go
@@ -14,7 +14,7 @@ func New[Value any](cmp comparator.Comparator[Value]) *Set[Value] {
 	return (*Set[Value])(immutable_rb_tree.New(cmp))
 }
 
-func NewSetFromValues[Value any](cmp comparator.Comparator[Value], values ...Value) *Set[Value] {
+func FromValues[Value any](cmp comparator.Comparator[Value], values ...Value) *Set[Value] {
 	return (*Set[Value])(immutable_rb_tree.FromValues(cmp, values...))
 }
 
